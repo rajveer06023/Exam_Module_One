0df411b124428fdfab9a4362a887453f9da95a3e
Rajveer patel <rajveerpatelbbu@gmail.com>
2025-11-14 22:59:14 +0530

    Part-B/ question 2(a)
    write a c program to print two digit number between 1 to 100. using for or while loop.

---
+++ range1to100.c
@@ -0,0 +1,21 @@
+//write a c program to print two digit number between 1 to 100. using while loop or for loop.
+
+#include<stdio.h>
+int main(){
+    int i = 10;  //decelered a variable 'i'
+
+
+    while(i <= 100)
+    {
+        printf("%d\n", i);
+
+
+        i++;
+    }
+
+
+
+    
+    
+    return 0;
+}
